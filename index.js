0282d7173b3a8cd106983e1bf37b0ead25347ff5
Craig Buchanan <craig.buchanan@cogniza.com>
2023-04-05 11:42:51 -0500

    reorganizing file

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const authentication = require('./authentication');
-// const getListStatesTrigger = require('./triggers/get_list_states.js');
+
 // const hireWorkerCreate = require('./creates/hire_worker.js');
+
 const findWorkerSearch = require('./searches/find_worker.js');
 
-module.exports = {
+// const getListStatesTrigger = require('./triggers/get_list_states.js');
+
+const App = {
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
   authentication: authentication,
+
+  beforeRequest: [],
+  afterResponse: [],
+
+  resources: {},
+
   // creates: { [hireWorkerCreate.key]: hireWorkerCreate },
-  // triggers: { [getListStatesTrigger.key]: getListStatesTrigger },
   searches: { [findWorkerSearch.key]: findWorkerSearch },
+  // triggers: { [getListStatesTrigger.key]: getListStatesTrigger },
 };
+
+module.exports = App;
